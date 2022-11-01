3466f47c3e4c8da6c7576660ee8deb2c2117ce74
Javaddosdev <113961204+Javaddosdev@users.noreply.github.com>
2022-11-01 22:08:36 +0700

    Update d0.c

--- d0.c
+++ d0.c
@@ -49,8 +49,8 @@ void broke(int s) {
 	// do nothing
 }
 
-#define CONNECTIONS 100
-#define THREADS 1000
+#define CONNECTIONS 20
+#define THREADS 200
 
 void attack(char *host, char *port, int id) {
 	int sockets[CONNECTIONS];
